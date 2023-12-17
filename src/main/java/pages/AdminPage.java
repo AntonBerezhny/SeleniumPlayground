cb2937a2b031c4c5f13603eea445a0cf5a9d78b9
AntonBerezhnyi <xaosdhx@gmail.com>
2023-12-17 20:05:02 +0200

    fixes reprorted by Alina

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -4,22 +4,25 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.util.List;
 
 public class AdminPage extends ParentPage{
     @FindBy(xpath = ".//span[@class='oxd-text oxd-text--span' and text()[contains(.,'Records Found')]]")
     private WebElement filterResultTitle;
-    @FindBy(xpath = ".//*[contains(text(), 'User Role')]//..//..//div[contains(text(), 'Select')]")
-    private WebElement userRoleDD;
-    @FindBy(xpath = ".//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")
-    private WebElement searchButton;
 
-    @FindBy(xpath = ".//*[@class='oxd-table-body']/div/div[@class='oxd-table-row oxd-table-row--with-border']/div[@role='cell'][3]")
+    @FindBy(xpath = ".//div[contains(@class, 'oxd-input-group oxd-input-field-bottom-space') and descendant::label[text()='User Role']]//div[contains(text(), 'Select')]")
+    private WebElement userRoleDD;
+    //xpath = ".//*[contains(text(), 'User Role')]//..//..//div[contains(text(), 'Select')]"
+    // "//div[contains(@class,'oxd-grid-item') and *//label[text()='User Role']]//div[@class='oxd-select-text-input']"
+
+    @FindBy(css = ".oxd-table-body>.oxd-table-card>.oxd-table-row>.oxd-table-cell:nth-of-type(3)")
     private List<WebElement> userRoleCellValue;
-    @FindBy(xpath = ".//button[@class='oxd-button oxd-button--medium oxd-button--ghost']")
+    // ".//*[@class='oxd-table-body']/div/div[@class='oxd-table-row oxd-table-row--with-border']/div[@role='cell'][3]")
+
+    @FindBy(xpath = ".//button[text()=' Reset ']")
     private WebElement resetButton;
+    @FindBy(xpath = ".//button[text()=' Search ']")
+    private WebElement searchButton;
 
     public AdminPage(WebDriver webDriver) {
         super(webDriver);
@@ -48,10 +51,12 @@ public class AdminPage extends ParentPage{
 
 
     public AdminPage checkSearchResultTableContainsEnteredValue(String userRole) {
+        waitSpinnerToHide();
         for (WebElement element: userRoleCellValue)
-            if (element.getText().contains(userRole)){
+            if (element.getText().equals(userRole)){
                 logger.info("cell contains " + userRole);
             }else {
+                logger.info(element.getText());
                 Assert.fail("cell doesn't contain " + userRole);
             }
 
