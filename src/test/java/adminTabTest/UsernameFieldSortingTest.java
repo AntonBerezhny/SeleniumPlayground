ec3e04436c4660b337d828639bafc0be964ace43
AntonBerezhnyi <xaosdhx@gmail.com>
2023-12-22 19:33:09 +0200

    push to main branch

---
+++ src/test/java/adminTabTest/UsernameFieldSortingTest.java
@@ -0,0 +1,15 @@
+package adminTabTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class UsernameFieldSortingTest extends BaseTest {
+@Test
+    public void sortingByUsernameColumn(){
+    homePage
+            .openHomePage()
+
+            ;
+}
+
+}
